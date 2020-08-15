62403c63326398108e1ec42bdc213ce84653f407
fuchun198435163.com <tank198435@163.com>
2020-08-15 22:12:30 +0800

    [fuchun] #tech spike some hutool str util

--- common/src/test/java/com/tank/HuToolsTest.java
+++ common/src/test/java/com/tank/HuToolsTest.java
@@ -5,22 +5,33 @@ import cn.hutool.cache.CacheUtil;
 import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.Zodiac;
+import cn.hutool.core.lang.WeightRandom;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONStrFormater;
 import com.google.common.base.MoreObjects;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.collect.Maps;
 import com.tank.util.vo.health.Health;
+import io.vavr.collection.Stream;
 import io.vavr.control.Try;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
+
+import static java.lang.String.format;
 
 /**
  * @author tank198435163.com
@@ -102,7 +113,7 @@ public class HuToolsTest {
     val prettyJsonStr = JSONStrFormater.format(jsonStr);
     System.out.println(prettyJsonStr);
 
-    val jsonClazz = Try.of(() -> this.getClass().getClassLoader().loadClass(PersonDto.class.getName()))
+    val jsonClazz = Try.of(() -> Thread.currentThread().getContextClassLoader().loadClass(PersonDto.class.getName()))
             .getOrElseThrow(() -> new ClassCastException("转换异常"));
 
     val converter = new JSONObject(jsonStr, true);
@@ -112,6 +123,79 @@ public class HuToolsTest {
     val age = DateUtil.date().between(birthDate).betweenYear(true);
     System.out.println(age);
   }
+
+  @Test
+  public void testGuess() {
+    val result = Double.doubleToLongBits(81.6f);
+    System.out.println(result);
+  }
+
+  @Test
+  public void testRandomWeight() {
+    val node1 = new NetworkNode().setIp("192.168.0.1").setPort(9000).setScore(1);
+    val node2 = new NetworkNode().setIp("192.168.0.2").setPort(9000).setScore(5);
+    val node3 = new NetworkNode().setIp("192.168.0.3").setPort(9000).setScore(10);
+    val weightRandom = new WeightRandom<NetworkNode>();
+    Stream.of(node1, node2, node3).forEach(node -> weightRandom.add(node, node.getScore()));
+    IntStream.rangeClosed(0, 3).boxed().forEach(index -> {
+      val result = weightRandom.next().toString();
+      System.out.println(result);
+    });
+    weightRandom.clear();
+  }
+
+  @Test
+  public void testTailMap() {
+    val map = Maps.<Integer, Integer>newTreeMap();
+    map.put(1, 2);
+    map.put(7, 3);
+    map.put(10, 6);
+    val result = map.tailMap(6, false);
+    System.out.println(result.firstKey());
+    System.out.println(map.lastKey());
+  }
+
+  @Test
+  @SneakyThrows
+  public void testCron() {
+    System.out.println(DateUtil.now());
+    val latch = new CountDownLatch(1);
+    val result = CronUtil.schedule("*/1 * * * * ? *", (Task) () -> {
+      System.out.println(DateUtil.now());
+      latch.countDown();
+    });
+    CronUtil.setMatchSecond(true);
+    CronUtil.start();
+    System.out.println(result);
+    latch.await();
+  }
+
+  @Test
+  @SneakyThrows
+  public void testGuavaCache() {
+    val cache = CacheBuilder.<String, String>newBuilder()
+            .maximumSize(200L)
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .refreshAfterWrite(100, TimeUnit.MILLISECONDS)
+            .weakKeys()
+            .removalListener(notification -> System.out.println(format("expire reason:%s", notification.getCause().name())))
+            .build(new CacheLoader<String, String>() {
+              @Override
+              @ParametersAreNonnullByDefault
+              public String load(@NonNull final String key) throws Exception {
+                return key;
+              }
+            });
+    cache.put("sz_distribution", "hello");
+    System.out.println(cache.get("sz_distribution"));
+  }
+
+  @Test
+  public void testStrUtil() {
+    val result = StrUtil.format("this is :{}, age is:{}", "jack", 21);
+    System.out.println(result);
+  }
+
 
   @Getter
   @Setter
@@ -131,6 +215,25 @@ public class HuToolsTest {
     private String birthday;
     private String zodiac;
     private String chineseZodiac;
+  }
+
+
+  @Getter
+  @Setter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Accessors(chain = true)
+  private static class NetworkNode {
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this).add("port", this.port)
+              .add("ip", this.ip).add("score", this.score).toString();
+    }
+
+    private Integer port;
+    private String ip;
+    private Integer score;
   }
 
   private int hash(Object value) {
